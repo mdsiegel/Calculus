9f998725edbdb309bc401396d4e54206e3abdd02
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-11-08 10:11:38 -0500

    Updated from Brython Server: 11/8/2017 10:11:37 AM

--- DerivativeProgram.py
+++ DerivativeProgram.py
@@ -3,9 +3,10 @@
 
 
 
-degree = int(input('How many terms do you have?'))
-for i in range(1,degree):
-    a = float(input('Enter the coefficient for x^'))
+terms = int(input('How many terms do you have?'))
+data = {}
+for i in range(1,terms):
+    data['coefficient':i] = float(input('Enter the coefficient for x^'))
 
 
 function = input('Enter a function')
